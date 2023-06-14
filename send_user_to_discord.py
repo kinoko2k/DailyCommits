8f10059497e28bf9be24badba4bc2a5dddda5012
Kinoko_2K <65443692+kinoko2k@users.noreply.github.com>
2023-06-14 23:17:34 +0900

    Update send_user_to_discord.py

--- send_user_to_discord.py
+++ send_user_to_discord.py
@@ -7,7 +7,6 @@ username = 'your_username'
 
 # 今日の日付と昨日の日付を取得する
 today = datetime.date.today().isoformat()
-yesterday = (datetime.date.today() - datetime.timedelta(days=1)).isoformat()
 
 # GitHub REST APIのエンドポイントURLを構築する
 url = f'https://api.github.com/users/{username}/events'
@@ -22,7 +21,6 @@ response = requests.get(url, headers=headers)
 # レスポンスから今日と昨日のコミット数を取得する
 events = response.json()
 today_commit_count = sum(1 for event in events if event['type'] == 'PushEvent' and event['created_at'].startswith(today))
-yesterday_commit_count = sum(1 for event in events if event['type'] == 'PushEvent' and event['created_at'].startswith(yesterday))
 
 # Discord Webhookに送信する準備
 webhook_url = os.environ['DISCORD_WEBHOOK']
@@ -30,7 +28,7 @@ data = {
     "embeds": [
         {
             "title": "今日のこみっと",
-            "description": f"今日のこみっと数：{commit_count}\n昨日のこみっと数：{yesterday_commit_count}",
+            "description": f"今日のこみっと数：{commit_count}",
             "color": 16711680  # 赤色
         }
     ]
