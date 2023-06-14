af258327e08be6ba2d82e31da87dd5f2ede73b01
Kinoko_2K <65443692+kinoko2k@users.noreply.github.com>
2023-06-14 23:19:59 +0900

    Update send_user_to_discord.py

--- send_user_to_discord.py
+++ send_user_to_discord.py
@@ -3,9 +3,9 @@ import datetime
 import os
 
 # ユーザー名を設定する
-username = 'your_username'
+username = 'kinoko2k'
 
-# 今日の日付と昨日の日付を取得する
+# 今日の日付を取得する
 today = datetime.date.today().isoformat()
 
 # GitHub REST APIのエンドポイントURLを構築する
@@ -18,9 +18,9 @@ access_token = os.environ['ACCESS_TOKEN']
 headers = {'Authorization': f'token {access_token}'}
 response = requests.get(url, headers=headers)
 
-# レスポンスから今日と昨日のコミット数を取得する
+# レスポンスから今日のコミット数を取得する
 events = response.json()
-today_commit_count = sum(1 for event in events if event['type'] == 'PushEvent' and event['created_at'].startswith(today))
+commit_count = sum(1 for event in events if event['type'] == 'PushEvent' and event['created_at'].startswith(today))
 
 # Discord Webhookに送信する準備
 webhook_url = os.environ['DISCORD_WEBHOOK']
